153f8dcf1e60381460c2711e506eaa1068498485
archanashokeeniitg <archanashokeeniitg@gmail.com>
2021-11-22 15:45:43 -0800

    removed extra text

--- src/views/pages/DataInsight/DataInsight.js
+++ src/views/pages/DataInsight/DataInsight.js
@@ -8,35 +8,28 @@ const DataInsight = () => {
     {
       id: 1,
       header: "Employees Salary",
-      description: "dolor sit amet,",
+      // description: "dolor sit amet,",
       image:
         "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/age.png",
     },
-    // {
-    //   id: 2,
-    //   header: "Employees Age",
-    //   description: "in metus quis tempor",
-    //   image:
-    //     "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/salary.png",
-    // },
     {
       id: 3,
       header: "Job Involvement Current Employees",
-      description: "condimentum purus,",
+      // description: "condimentum purus,",
       image:
         "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/jobinvolvement_employed.png",
     },
     {
       id: 4,
       header: "Job Satisfaction Current Employees",
-      description: "vitae neque",
+      // description: "vitae neque",
       image:
         "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/jobsatisfaction_employed.png",
     },
     {
       id: 5,
       header: "Employees leaving By department",
-      description: "finibus id eros eu,",
+      // description: "finibus id eros eu,",
       image:
         "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/left_bydept.png",
     },
@@ -50,35 +43,35 @@ const DataInsight = () => {
     {
       id: 7,
       header: "Employees leaving By Job Role",
-      description: "finibus id eros eu,",
+      // description: "finibus id eros eu,",
       image:
         "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/left_jobrole.png",
     },
     {
       id: 8,
       header: "Breakdown for Lab Technicians",
-      description: "finibus id eros eu,",
+      // description: "finibus id eros eu,",
       image:
         "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/LT_brkdown.png",
     },
     {
       id: 9,
       header: "Breakdown for Research Assistants",
-      description: "finibus id eros eu,",
+      // description: "finibus id eros eu,",
       image:
         "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/RA_brkdown.png",
     },
     {
       id: 10,
       header: "Breakdown for Sales Executive",
-      description: "finibus id eros eu,",
+      // description: "finibus id eros eu,",
       image:
         "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/Sales_Exec_Breakdown.png",
     },
     {
       id: 11,
       header: "Breakdown for Sales Representative ",
-      description: "finibus id eros eu,",
+      // description: "finibus id eros eu,",
       image:
         "https://sagemaker-mtp-hr.s3.us-east-2.amazonaws.com/jupyter-output/Sales_rep_breakdown.png",
     },
